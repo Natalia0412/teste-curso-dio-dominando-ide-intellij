34d6a43e6fec1936369d672538c542d9644d3230
NataliaMonteiroB <natalia412@outlook.com.br>
2021-10-28 23:14:51 -0200

    Criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,6 +1,7 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
 
 
 public class PrimeiroPrograma {
@@ -11,5 +12,8 @@ public class PrimeiroPrograma {
 
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro1=new Livro("A culpa é das estrelas",250);
+        System.out.println(livro1);
     }
 }
